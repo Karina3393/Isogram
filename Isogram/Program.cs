1bbaf25b57f57d0abc1f935e705a9de4de0d6c74
карина <@DESKTOP-FP9I0KA>
2024-09-26 14:48:34 +0300

    Add project files.

---
+++ Isogram/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Isogram
+{
+    internal class Program
+    {
+        static void Main()
+        {
+          
+            Console.WriteLine("Введіть слово або фразу:");
+            string input = Console.ReadLine().ToLower(); 
+           
+            string filteredInput = new string(input.Where(c => char.IsLetter(c)).ToArray());
+
+            
+            bool isIsogram = filteredInput.Distinct().Count() == filteredInput.Length;
+
+            
+            if (isIsogram)
+            {
+                Console.WriteLine("Це ізограма.");
+            }
+            else
+            {
+                Console.WriteLine("Це не ізограма.");
+            }
+        }
+    }
+}
